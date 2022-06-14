dc386e48588940565d7d332df9b905ae17e3c3f9
Rutushree9 <rutushreeagarwal@gmail.com>
2022-06-14 13:02:08 +0530

    Initial Push

--- Revolve/python_assignment_final/solution/solution_start.py
+++ Revolve/python_assignment_final/solution/solution_start.py
@@ -8,12 +8,12 @@ def get_params() -> dict:
     Obtain the data locations using argument parser
     """
     # Logging
-    print("Capturing the locations for the data")
+    print("\n\n Capturing the locations for the data")
     parser = argparse.ArgumentParser(description='DataTest')
-    parser.add_argument('--customers_location', required=False, default="./input_data/starter/customers.csv")
-    parser.add_argument('--products_location', required=False, default="./input_data/starter/products.csv")
-    parser.add_argument('--transactions_location', required=False, default="./input_data/starter/transactions/")
-    parser.add_argument('--output_location', required=False, default="./output_data/outputs/")
+    parser.add_argument('--customers_location', required=False, default="C://Users//91797//Desktop//Revolve_1//Revolve//input_data//starter//customers.csv")
+    parser.add_argument('--products_location', required=False, default="C://Users//91797/Desktop//Revolve_1//Revolve//input_data//starter//products.csv")
+    parser.add_argument('--transactions_location', required=False, default="C://Users//91797//Desktop//Revolve_1//Revolve//input_data//starter//transactions//")
+    parser.add_argument('--output_location', required=False, default="C://Users//91797//Desktop//Revolve_1//Revolve//output_data//outputs//")
     return vars(parser.parse_args())
 
 
@@ -23,6 +23,8 @@ def get_cust_data(params:dict) -> pd.DataFrame:
     Parameters:
     params : Dictionary containing all the data addresses
     """
+    # Logging
+    print("\n\n Obtaining Customer Data:\n")
     try:
         return pd.read_csv(params["customers_location"])
     except FileNotFoundError:
@@ -35,6 +37,8 @@ def get_product_data(params:dict) -> pd.DataFrame:
     Parameters:
     params : Dictionary containing all the data addresses
     """
+    # Logging
+    print("\n\n Obtaining Product Data:\n")
     try:
         return pd.read_csv(params["products_location"])
     except FileNotFoundError:
@@ -47,19 +51,21 @@ def get_transaction_data(params:dict) -> pd.DataFrame:
     Parameters:
     params : Dictionary containing all the data addresses
     """
+    # Logging
+    print("\n\n Obtaining Transactions Data:\n")
     try:
         path_to_json = params["transactions_location"]
 
-        # Gather all the JSON files
+            # Gather all the JSON files
         dfList = []
         jsonFiles = glob(f'{path_to_json}/**/*.json') 
 
-        # Logging
-        print("Reading the JSON files")
+            # Logging
+        print("\n\n Reading the JSON files")
         for jsonFile in jsonFiles:
-            df = pd.read_json(jsonFile, lines = True)
-            dfList.append(df)
-            
+                df = pd.read_json(jsonFile, lines = True)
+                dfList.append(df)
+                
         dfTrainingDF = pd.concat(dfList, axis=0)
         cust_id_data = pd.concat([pd.DataFrame(x) for x in dfTrainingDF['basket']], keys=dfTrainingDF['customer_id']).reset_index(level=1, drop=True).reset_index()
         purchase_date_data = pd.concat([pd.DataFrame(x) for x in dfTrainingDF['basket']], keys=dfTrainingDF['date_of_purchase']).reset_index(level=1, drop=True).reset_index()
@@ -76,10 +82,13 @@ def merged_data(params:dict) -> pd.DataFrame:
     """
     try:
         # Logging
-        print("Merging the data")
+        print("\n Merging the data")
         customer_data = get_cust_data(params)
+        print("\n customer_data: \n",customer_data.head() )
         product_data = get_product_data(params)
+        print("\n product_data: \n",product_data.head() )
         transactions_data = get_transaction_data(params)
+        print("\n transactions_data:\n",transactions_data.head() )
         cust_trans_data = customer_data.merge(transactions_data, on="customer_id")
         data = pd.DataFrame(cust_trans_data.merge(product_data, on = "product_id"))
         return data.groupby(["customer_id", 
@@ -95,7 +104,8 @@ def main() -> None:
     """
     params = get_params()
     final_data = merged_data(params)
-    final_data.to_csv(f'{params["output_location"]}/final_outcome.csv', index=False)
+    print("\n\n final_data:\n", final_data.head())
+    final_data.to_csv(f'{params["output_location"]}//final_outcome.csv', index=False)
 
 if __name__ == "__main__":
     main()
